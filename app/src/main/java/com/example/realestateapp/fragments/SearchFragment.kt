1a0b253c1ca331d6d10b815631bb7654c99047d0
DenxyChen <denxychen@gmail.com>
2022-04-20 12:01:32 -0700

    search is dynamic, search layout updated

--- app/src/main/java/com/example/realestateapp/fragments/SearchFragment.kt
+++ app/src/main/java/com/example/realestateapp/fragments/SearchFragment.kt
@@ -22,10 +22,10 @@ import org.json.JSONException
 
 class SearchFragment : Fragment() {
     private val client = AsyncHttpClient()
-    private val headers = RequestHeaders()
     private val listings = mutableListOf<Listing>()
 
-    lateinit var etSearchQuery: EditText
+    lateinit var etState: EditText
+    lateinit var etCity: EditText
     lateinit var btnSearch: Button
 
     private val sharedViewModel: SharedViewModel by activityViewModels()
@@ -41,27 +41,25 @@ class SearchFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
-        etSearchQuery = view.findViewById(R.id.etSearchQuery)
+        etState = view.findViewById(R.id.etState)
+        etCity = view.findViewById(R.id.etCity)
         btnSearch = view.findViewById(R.id.btnSearch)
-
-        sharedViewModel.listings.observe(viewLifecycleOwner) {
-            etSearchQuery.setText("")
-        }
 
         btnSearch.setOnClickListener {
             getSearchResults()
         }
-
-        headers.put("X-RapidAPI-Host", getString(R.string.rapid_api_host))
-        headers.put("X-RapidAPI-Key", getString(R.string.rapid_api_key))
     }
 
     private fun getSearchResults() {
+        val headers = RequestHeaders()
+        headers.put("X-RapidAPI-Host", getString(R.string.rapid_api_host))
+        headers.put("X-RapidAPI-Key", getString(R.string.rapid_api_key))
+
         val params = RequestParams()
+        params.put("city", etCity.text.toString())
+        params.put("state_code", etState.text.toString())
         params.put("offset", "0")
         params.put("limit", "42")
-        params.put("state_code", "MI")
-        params.put("city", "Detroit")
         params.put("sort", "newest")
         params.put("property_type", "multi_family,single_family")
 
@@ -69,11 +67,17 @@ class SearchFragment : Fragment() {
             override fun onSuccess(statusCode: Int, headers: Headers?, json: JSON) {
                 Log.i(TAG, "onSuccess: JSON data $json")
                 try {
+                    // Parse JSON result into Listing objects and save to ViewModel mutable list
                     val dataJson = json.jsonObject.getJSONObject("data")
                     val listingJsonArray = dataJson.getJSONObject("home_search").getJSONArray("results")
                     listings.addAll(Listing.fromJsonArray(listingJsonArray))
                     sharedViewModel.saveListings(listings)
                     Log.i(TAG, "Listing list $listings")
+
+                    // Swap to results screen
+                    val transaction = parentFragmentManager.beginTransaction()
+                    transaction.replace(R.id.flContainer, ResultsFragment())
+                    transaction.commit()
                 } catch (e: JSONException) {
                     Log.e(TAG, "Encountered exception $e")
                 }
