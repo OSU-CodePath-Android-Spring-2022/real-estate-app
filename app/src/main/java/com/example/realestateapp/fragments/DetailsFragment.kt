33aa04fe1258aaa9968016c3ec75ac7f58317862
aidangold <aidanpaulgoldstein@gmail.com>
2022-04-21 14:54:27 -0400

    updates to Details, almost ready

--- app/src/main/java/com/example/realestateapp/fragments/DetailsFragment.kt
+++ app/src/main/java/com/example/realestateapp/fragments/DetailsFragment.kt
@@ -1,16 +1,29 @@
 package com.example.realestateapp.fragments
 
 import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.fragment.app.activityViewModels
+import com.example.realestateapp.R
+import com.example.realestateapp.models.SharedViewModel
+
 
 class DetailsFragment: Fragment() {
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-//        MainPhoto = findViewById(R.id.MainPhoto)
-//        tvPrice = findViewById(R.id.tvPrice)
-//        tvBedRooms = findViewById(R.id.tvBedRooms)
-//        tvBathRooms = findViewById(R.id.tvBathRooms)
-//        PhotoScroll = findViewById(R.id.PhotoScroll)
+    private val sharedViewModel: SharedViewModel by activityViewModels()
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // inflate the layout for the details fragment
+        return inflater.inflate(R.layout.details_screen, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        sharedViewModel.listings.observe(viewLifecycleOwner) {}
     }
 }
