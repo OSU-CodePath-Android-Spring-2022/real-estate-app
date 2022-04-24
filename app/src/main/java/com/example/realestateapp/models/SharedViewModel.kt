f282f9bb069cd040d7c0052448b41f8fd664ca2f
DenxyChen <denxychen@gmail.com>
2022-04-24 05:03:09 -0700

    results persists across startups, storage is cleared on uninstall, theme changed, action bar hidden

--- app/src/main/java/com/example/realestateapp/models/SharedViewModel.kt
+++ app/src/main/java/com/example/realestateapp/models/SharedViewModel.kt
@@ -3,14 +3,18 @@ package com.example.realestateapp.models
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import java.security.AccessController.getContext
 
 class SharedViewModel: ViewModel() {
-
     private var _listings = MutableLiveData<MutableList<Listing>>()
     val listings: LiveData<MutableList<Listing>> = _listings
 
     private var _listing = MutableLiveData<Listing>()
     val listing: LiveData<Listing> = _listing
+
+    private var _initialLoad = MutableLiveData(true)
+    val initialLoad: LiveData<Boolean> = _initialLoad
+
 
     fun saveListings(newListings: MutableList<Listing>) {
         _listings.value = newListings
@@ -18,5 +22,9 @@ class SharedViewModel: ViewModel() {
 
     fun saveListing(newListing: Listing) {
         _listing.value = newListing
+    }
+
+    fun onLoadSuccess() {
+        _initialLoad.value = false
     }
 }
